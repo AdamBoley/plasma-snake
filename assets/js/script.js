33baccdfd5a01cef17bf14f57dcad3465e6884f2
AdamBoley <adamboley92@gmail.com>
2023-03-15 16:57:24 +0000

    jed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,26 @@
+// let button = document.getElementById('button')
+// button.addEventListener('click', reveal)
+
+// function reveal() {
+//     let buttonContent = document.getElementById('button-content')
+
+//     buttonContent.style.display = 'block'
+// }
+
 let button = document.getElementById('button')
 button.addEventListener('click', reveal)
 
-function reveal() {
-    let buttonContent = document.getElementById('button-content')
+let buttonHide = document.getElementById('button-hide')
+buttonHide.addEventListener('click', hide)
 
-    buttonContent.style.display = 'block'
+function reveal() {
+    let content = document.getElementById('jed')
+
+    content.style.display = 'block'
 }
 
+function hide(){
+    let content = document.getElementById('jed')
+
+    content.style.display = 'none'
+}
